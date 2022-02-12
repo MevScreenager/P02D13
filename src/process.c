253f63c44781f99860388b1553efe67e1a13750e
Michale Hound <michaleh@su-g5.msk.21-school.ru>
2022-02-12 19:39:50 +0300

    process

--- src/process.c
+++ src/process.c
@@ -4,61 +4,71 @@
 //field [m][n]
 
 int main() {
-
+    
+        }
+    }
+    
 }
 
 int process(char **field, char d, char l) {
     for (int k = 0; k < 25; k++) {
         for (int t = 0; t< 80; t++) {
             if (field[k][t] = d) {
-                dead(field[k][t]);
+                dead(field[k][t], **field);
                 }
             if (field[k][t] = l) {
-                live(field[k][t]);
+                live(field[k][t], **field);
             }
         }
     } return 0;
 }
 
-void dead(char field[m][n], char d, char l) {
+void dead(char field[m][n], char **field) {
     int count = 0;
-    if (field[m][n] = d) {
-        if (m = 0; )
-        for (int i = m - 1; i <= m + 1; i++) {
-            for (int j = n - 1; j <= n + 1; j++) {
-                if (i < 0) {
-                    i = 24;
-                }
-                if (i > 24) {
-                    i = 0;
-                }
-                if (j < 0) {
-                    j = 79:
-                }
-                if (j > 79) {
-                    j = 0:
-                }
-                if (field [i][j] = l) {
-                    count++;
-                }
+    for (int i = m - 1; i <= m + 1; i++) {
+        for (int j = n - 1; j <= n + 1; j++) {
+            if (i < 0) {
+                i = 24;
             }
-        } if (count = 3) {
-            field[m][n] = l;
+            if (i > 24) {
+                i = 0;
+            }
+            if (j < 0) {
+                j = 79;
+            }
+            if (j > 79) {
+                j = 0;
+            }
+            if (field [i][j] = l) {
+                count++;
+            }
         }
+    } if (count = 3) {
+        field[m][n] = l;
     }
 }
 
-void live(char field[m][n], char d, char l) {
+void live(char field[m][n], char **field) {
     int count = -1;
-    if (field[m][n] = l) {
-        for (int i = m - 1; i <= m + 1; i++) {
-            for {int j = n - 1}; j <= n + 1; j++) {
-                if (field[i][j] = l) {
-                    count++;
-                }
+    for (int i = m - 1; i <= m + 1; i++) {
+        for {int j = n - 1}; j <= n + 1; j++) {
+            if (i < 0) {
+                i = 24;
             }
-        } if (count < 2 && count > 3) {
-            field[m][n] = d;
+            if (i > 24) {
+                i = 0;
+            }
+            if (j < 0) {
+                j = 79:
+            }
+            if (j > 79) {
+                j = 0:
+            }
+            if (field[i][j] = l) {
+                count++;
+            }
         }
+    } if (count < 2 || count > 3) {
+            field[m][n] = d;
     }
 }
