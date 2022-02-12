f2d1604fb3e7e23e3c75c0df67d47b6113f3a2c9
Michale Hound <michaleh@su-g5.msk.21-school.ru>
2022-02-12 19:02:24 +0300

    process

--- src/process.c
+++ src/process.c
@@ -23,8 +23,21 @@ int process(char **field, char d, char l) {
 void dead(char field[m][n], char d, char l) {
     int count = 0;
     if (field[m][n] = d) {
+        if (m = 0; )
         for (int i = m - 1; i <= m + 1; i++) {
             for (int j = n - 1; j <= n + 1; j++) {
+                if (i < 0) {
+                    i = 24;
+                }
+                if (i > 24) {
+                    i = 0;
+                }
+                if (j < 0) {
+                    j = 79:
+                }
+                if (j > 79) {
+                    j = 0:
+                }
                 if (field [i][j] = l) {
                     count++;
                 }
