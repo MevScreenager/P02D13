7a8a54d75717f6372296c9dc7ee0416e471d0496
Arryn Pallas <arrynpal@su-g1.msk.21-school.ru>
2022-02-12 17:42:29 +0300

    n

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -2,22 +2,26 @@
 
 void draw(char **field);    //отрисовка поля
 void add(char **field, char **start, int x, int y, int lx, int ly);   //добавляем стартовую позицию
+void in_old(char **field, char **field_old);
 int process(char **field); //процесс игры
 int input();
 
 int main() {
     int x, y, lx = 20, ly = 10;
-    char field[25][80], start[ly][lx];
+    char field[25][80], start[ly][lx], field_old[25][80];
     add((char **) field, (char **) start, x, y, lx, ly);
     draw((char **) field);
+    in_old((char **) field, (char **) field_old);
     while (1) {
         if (input()) {
-            process((char **) field);
-            draw((char **) field);
+            if (process((char **) field)) {
+                in_old((char **) field, (char **) field_old);
+                draw((char **) field);
+            } else {
+                return 0;
+            }
         }
-        else
-            draw((char **) field);
-        
+        draw((char **) field);
     }
 
 }
@@ -34,6 +38,12 @@ int input() {
         flag = 1;
     }
      return flag;
+}
+
+void in_old(char **field, char **field_old) {
+    for (int i = 0; i < 25; i++)
+        for (int j = 0; j < 80; j++)
+            field_old[i][j] = field[i][j];
 }
 
 void add(char **field, char **start, int x, int y, int lx, int ly) {}
