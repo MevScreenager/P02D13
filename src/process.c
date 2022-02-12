04b4c1dd29bb2061bb8ddc4efae5cd9ae6f216a4
Michale Hound <michaleh@su-g5.msk.21-school.ru>
2022-02-12 18:25:43 +0300

    process

--- src/process.c
+++ src/process.c
@@ -8,23 +8,22 @@ int main() {
 }
 
 int process(char **field, char d, char l) {
-    int count = 0;
-    for (int k = 0; k < m; k++) {
-        for (int t = 0; t< n; t++) {
-            if (field[m][n] = d) {
-                dead(field[m][n]);
+    for (int k = 0; k < 25; k++) {
+        for (int t = 0; t< 80; t++) {
+            if (field[k][t] = d) {
+                dead(field[k][t]);
                 }
-            if (field[m][n] = l) {
-                live(field[m][n]);
+            if (field[k][t] = l) {
+                live(field[k][t]);
             }
         }
     }
 }
 
-void dead(char **field, char d, char l) {
-    int count++;
+void dead(char field[m][n], char d, char l) {
+    int count = 0;
     if (field[m][n] = d) {
-        for (int i= m - 1; i <= m + 1; i++) {
+        for (int i = m - 1; i <= m + 1; i++) {
             for (int j = n - 1; j <= n + 1; j++) {
                 if (field [i][j] = l) {
                     count++;
@@ -32,6 +31,21 @@ void dead(char **field, char d, char l) {
             }
         } if (count = 3) {
             field[m][n] = l;
+        }
+    }
+}
+
+void live(char field[m][n], char d, char l) {
+    int count = -1;
+    if (field[m][n] = l) {
+        for (int i = m - 1; i <= m + 1; i++) {
+            for {int j = n - 1}; j <= n + 1; j++) {
+                if (field[i][j] = l) {
+                    count++;
+                }
+            }
+        } if (count < 2 && count > 3) {
+            field[m][n] = d;
         }
     }
 }
